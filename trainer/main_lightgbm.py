a5a055f312a31df757bb1e66123d84547da0f531
sophiearana <arana.sophie@gmail.com>
2018-04-25 17:00:44 +0200

    converted to scilearn api

--- trainer/main_lightgbm.py
+++ trainer/main_lightgbm.py
@@ -19,37 +19,16 @@ import time
 import lightgbm as lgb
 import numpy as np
 import pandas as pd
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
 
 import preprocessing as pp
 
-def lgb_modelfit(params, dtrain, dvalid, predictors, target='target',
+def lgb_modelfit(dtrain, dvalid, predictors, target='target',
                       objective='binary', metrics='auc', feval=None, 
                       early_stopping_rounds=20, num_boost_round=3000, 
                       verbose_eval=10, categorical_features=None):
-    lgb_params = {
-        'boosting_type': 'gbdt',
-        'objective': objective,
-        'metric':metrics,
-        'learning_rate': 0.08,
-        'num_leaves': 31,  # we should let it be smaller than 2^(max_depth)
-        'max_depth': -1,  # -1 means no limit
-        'min_child_samples': 20,  # Minimum number of data need in a child(min_data_in_leaf)
-        'max_bin': 255,  # Number of bucketed bin for feature values
-        'subsample': 0.6,  # Subsample ratio of the training instance.
-        'subsample_freq': 0,  # frequence of subsample, <=0 means no enable
-        'colsample_bytree': 0.3,  # Subsample ratio of columns when constructing each tree.
-        'min_child_weight': 5,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
-        'subsample_for_bin': 200000,  # Number of samples for constructing bin
-        'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
-        'reg_alpha': 0,  # L1 regularization term on weights
-        'reg_lambda': 0,  # L2 regularization term on weights
-        'nthread': 8,
-        'verbose': 0,
-        'metric':metrics
-    }
 
-    lgb_params.update(params)
 
     print("Preparing validation datasets...")
 
@@ -66,24 +45,38 @@ def lgb_modelfit(params, dtrain, dvalid, predictors, target='target',
     evals_results = {}
 
     # Optimization should loop around this line lightgbm.cv
-    bst = lgb.cv(lgb_params, xgtrain, num_boost_round=num_boost_round, folds=None, nfold=5, stratified=True,
-                      shuffle=True,
-                      metrics=metrics, fobj=None, feval=None, init_model=None, feature_name=predictors,
-                      categorical_feature=categorical_features,
-                      early_stopping_rounds=early_stopping_rounds, fpreproc=None, verbose_eval=10, show_stdv=True,
-                      seed=7, callbacks=None)
+    gbm = lgb.LGBMRegressor(boosting_type='gbdt',
+                            learning_rate=0.2,
+                            metric=metrics,
+                            num_leaves=1400,  # we should let it be smaller than 2^(max_depth)
+                            max_depth=3,  # -1 means no limit
+                            min_child_samples=100,  # Minimum number of data need in a child(min_data_in_leaf)
+                            max_bin=100,  # Number of bucketed bin for feature values
+                            subsample=.8,  # Subsample ratio of the training instance.
+                            subsample_freq=1,  # frequence of subsample, <=0 means no enable
+                            colsample_bytree=0.9,  # Subsample ratio of columns when constructing each tree.
+                            min_child_weight=0,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+                            sumbsample_for_bin=200000,
+                            min_split_gain=0,
+                            reg_alpha=0,
+                            reg_lambda=0,
+                            nthread=8,
+                            verbose=0,
+                            scale_pos_weight=80,
+                            objective=objective,
+                            num_boost_round=num_boost_round)
+
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=7)
+    bst = cross_val_score(gbm, dtrain[predictors], dtrain[target],
+                          groups=None, scoring='roc_auc', cv=skf, n_jobs=1, verbose=1)
 
     # then optimal parameters should be chosen here based on mean auc over folds
-    array = np.array(bst["auc-mean"], dtype=float)
-    array.mean()
+    bst.mean()
     # and then model retrained with those on full training set.
     # (eventually we will replace the xgtrain and xgvalid split by our own 90-10 split)
 
-    bst1 = lgb.train(lgb_params, xgtrain, valid_sets=[xgtrain, xgvalid], 
-                     valid_names=['train','valid'], evals_result=evals_results, 
-                     num_boost_round=num_boost_round, 
-                     early_stopping_rounds=early_stopping_rounds, 
-                     verbose_eval=10, feval=feval)
+    bst1 = gbm.fit(dtrain[predictors], dtrain[target], eval_set=[(dvalid[predictors], dvalid[target])],
+                   eval_metric=metrics, early_stopping_rounds=early_stopping_rounds)
 
     n_estimators = bst1.best_iteration
     print("\nModel Report")
@@ -120,8 +113,8 @@ def main(train_file, test_file, job_dir):
         'scale_pos_weight':80
     }
     
-    bst = lgb_modelfit(params, train_df, val_df, predictors, target,
-                            objective='binary', metrics='auc', 
+    bst = lgb_modelfit(train_df, val_df, predictors, target,
+                            objective='binary', metrics='auc',
                             early_stopping_rounds=40, verbose_eval=True, 
                             num_boost_round=500, 
                             categorical_features=categorical)
