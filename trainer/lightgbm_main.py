6a9bb5852f80393ef0a0dddd6ea04ad262066ad0
Johanna de Vos <johannadevos@gmail.com>
2018-06-06 11:45:42 +0200

    Further worked on specifying the hyperparameter values (both the defaults, and the values that should be optimized through grid search).

--- trainer/lightgbm_main.py
+++ trainer/lightgbm_main.py
@@ -34,24 +34,26 @@ import trainer.preprocessing as pp
 
 # Default parameters
 LGBM_PARAMS = {
-    'boosting_type':      'gbdt',
-    'objective':          'binary',
-    'metric':             'auc',
-    'learning_rate':      0.08,
-    'num_leaves':         31,  # we should let it be smaller than 2^(max_depth)
-    'max_depth':          -1,  # -1 means no limit
-    'min_child_samples':  20,  # Minimum number of data need in a child(min_data_in_leaf)
-    'max_bin':            255,  # Number of bucketed bin for feature values
-    'subsample':          0.6,  # Subsample ratio of the training instance.
-    'subsample_freq':     0,  # frequence of subsample, <=0 means no enable
-    'colsample_bytree':   0.3,  # Subsample ratio of columns when constructing each tree.
-    'min_child_weight':   5,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
-    'subsample_for_bin':  200000,  # Number of samples for constructing bin
-    'min_split_gain':     0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
-    'reg_alpha':          0,  # L1 regularization term on weights
-    'reg_lambda':         0,  # L2 regularization term on weights
-    'nthread':            8,
-    'verbose':            0,
+    'boosting_type':      'gbdt', # Gradient Boosting Decision Tree
+    'objective':          'binary', # Binary classification
+    'metric':             'auc', # Area under the ROC curve
+    'learning_rate':      0.08, # Controls how much each tree is weighted
+    'num_leaves':         31, # Max number of leaves per tree (should not exceed 2^max_depth)
+    'min_data_in_leaf':   20, # Min number of data points per leaf
+    'max_depth':          -1, # Tree depth. -1 means no limit
+    'max_bin':            255, # How memory will be auto-compressed (number of bucketed bins for feature values)
+    'subsample':          0.6, # Ratio of observations that are randomly sampled per tree
+    'subsample_freq':     0, # How often bagging should happen
+    'colsample_bytree':   0.3, # Subsample ratio of columns when constructing each tree.
+    'min_child_weight':   5, # Minimum sum of instance weight(hessian) needed in a child(leaf)
+    'subsample_for_bin':  200000, # Number of samples for constructing histogram bins
+    'min_split_gain':     0, # Minimum loss reduction needed for a node to split
+    'reg_alpha':          0, # L1 regularization term on weights
+    'reg_lambda':         0, # L2 regularization term on weights
+    'nthread':            8, # Number of threads
+    'verbose':            0, # Verbosity
+    'n_estimators':       99999999, # Number of boosting iterations. Very high because of early stopping
+	 'scale_pos_weight':   1, # Weight of the positive class in binary classification
 }
 
 
